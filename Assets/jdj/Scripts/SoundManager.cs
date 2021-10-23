270f6e1cbe0b367e1f714a9c089709d7babcc2a6
kojdj0811 <kojdj0811@gmail.com>
2021-10-24 07:51:17 +0900

    사운드 적용 Finish 상태 추가

--- Assets/jdj/Scripts/SoundManager.cs
+++ Assets/jdj/Scripts/SoundManager.cs
@@ -12,21 +12,27 @@ public class SoundManager : MonoBehaviour
 
 
 
+
     private void Awake() {
-        if (S == null)
-            S = this;
+        S = this;
     }
 
 
     public void PlaySound_ButtonMouseHover () {
-        Destroy(Instantiate(mouseHoverSound), 1.0f);
+        var go = Instantiate(mouseHoverSound);
+        DontDestroyOnLoad(go);
+        Destroy(go, 1.0f);
     }
 
     public void PlaySound_ButtonMouseDown () {
-        Destroy(Instantiate(mouseClickSound), 1.0f);
+        var go = Instantiate(mouseClickSound);
+        DontDestroyOnLoad(go);
+        Destroy(go, 1.0f);
     }
 
     public void PlaySound_ButtonMouseUp () {
-        Destroy(Instantiate(mouseClickSound), 1.0f);
+        var go = Instantiate(mouseClickSound);
+        DontDestroyOnLoad(go);
+        Destroy(go, 1.0f);
     }
 }
