fc97affe8ed6f6a7454314d087e9e10cc6dd7b35
kojdj0811 <kojdj0811@gmail.com>
2021-10-24 11:24:51 +0900

    얍

--- Assets/jdj/Scripts/Shield.cs
+++ Assets/jdj/Scripts/Shield.cs
@@ -4,17 +4,26 @@ using UnityEngine;
 
 public class Shield : MonoBehaviour
 {
+    public GameObject effectSound;
+
     public int lifeMax = 2;
     [SerializeField]
     private int life = 0;
     public int Life {
         get => life;
         set {
-            life = value;
+
+            if(life > value)
+                Destroy(Instantiate(effectSound), 1.0f);
+
+
             if(life > lifeMax)
                 life = lifeMax;
             if(life < 0)
                 life = 0;
+
+
+            life = value;
 
             gameObject.SetActive(life > 0);
 
